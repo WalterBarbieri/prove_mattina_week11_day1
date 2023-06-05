5a8971f90419f03af56d184502ab6d14becdb1d9
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-06-05 10:06:02 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'prove_mattina_week11_day1';
+}
